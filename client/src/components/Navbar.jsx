3f98fa5909c26f19d1d6c65b57b74d74d326ce77
TanKittipat <kittipat12tan@gmail.com>
2025-03-13 12:39:48 +0700

    Edit Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import { MessageSquare } from "lucide-react";
+import { MessageSquare, User, LogOut, Settings } from "lucide-react";
+import { useAuthStore } from "../stores/useAuthStore";
 
 const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
   return (
     <header className="navbar bg-base-100 shadow-md">
       <div className="navbar-start">
@@ -12,11 +14,20 @@ const Navbar = () => {
       <div className="navbar-end">
         <div className="gap-2">
           <a href="/signup" className="btn">
-            Sign Up
+            <Settings className="size-4" /> Settings
           </a>
-          <a href="/signin" className="btn">
-            Sign In
-          </a>
+          {authUser && (
+            <>
+              <a href="/profile" className="btn">
+                <User className="size-4" />
+                Profile
+              </a>
+              <button className="btn">
+                <LogOut className="size-4" />
+                Logout
+              </button>
+            </>
+          )}
         </div>
       </div>
     </header>
