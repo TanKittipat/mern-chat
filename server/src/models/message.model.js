789847fa5e94db110d51c81815cb9ca28d5736f0
TanKittipat <kittipat12tan@gmail.com>
2025-03-13 10:07:41 +0700

    Add timestamps

--- server/src/models/message.model.js
+++ server/src/models/message.model.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const messageSchema = new Schema({
-  senderId: { type: Schema.Types.ObjectId, ref: "User" },
-  receiverId: { type: Schema.Types.ObjectId, ref: "User" },
-  text: { type: String, required: true },
-  image: { type: String },
-});
+const messageSchema = new Schema(
+  {
+    senderId: { type: Schema.Types.ObjectId, ref: "User" },
+    receiverId: { type: Schema.Types.ObjectId, ref: "User" },
+    text: { type: String, required: true },
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const MessageModel = mongoose.model("Message", messageSchema);
