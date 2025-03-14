42300885839779998b82dc9813fb80d43b46b18d
TanKittipat <kittipat12tan@gmail.com>
2025-03-14 09:10:25 +0700

    Bug redirect

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -2,24 +2,11 @@ import { Outlet } from "react-router";
 import "./layout.css";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
-import { useAuthStore } from "../stores/useAuthStore";
 import { useThemeStore } from "../stores/useThemeStore";
-import { useEffect } from "react";
-import { Loader } from "lucide-react";
 
 const MainLayout = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   const { theme } = useThemeStore();
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-  if (isCheckingAuth && authUser) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
-  }
+
   return (
     <div data-theme={theme || "dark"}>
       <Navbar />
--- client/src/routers/redirect.jsx
+++ client/src/routers/redirect.jsx
@@ -1,8 +1,22 @@
 import { useAuthStore } from "../stores/useAuthStore";
 import { Navigate } from "react-router";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 const LoginRedirect = ({ children }) => {
-  const { authUser } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   if (!authUser) {
     return <Navigate to={"/signin"} />;
